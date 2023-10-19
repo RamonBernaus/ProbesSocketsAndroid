0abca414cff46973a6db6dc502aee9129d3c8d0a
Ramon <a21rambertor@inspedralbes.cat>
2023-10-19 13:19:43 +0200

    Xat fet (?)

---
+++ app/src/main/java/com/example/probes_socketio/Constants.java
@@ -0,0 +1,6 @@
+package com.example.probes_socketio;
+
+public class Constants {
+        public static final String CHAT_SERVER_URL = "https://socket-io-chat.now.sh/";
+}
+
--- app/src/main/java/com/example/probes_socketio/MainActivity.java
+++ app/src/main/java/com/example/probes_socketio/MainActivity.java
@@ -18,65 +18,11 @@ import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
 
 public class MainActivity extends AppCompatActivity {
-    /*private EditText mInputMessageView;
-    private Socket mSocket;
-
-    {
-        try {
-            mSocket = IO.socket("http://chat.socket.io");
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-
-        //mSocket.connect();
-        //mSocket.on("new message", onNewMessage);
-        //mSocket.connect();
     }
-
-    /*
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-
-        mSocket.disconnect();
-        mSocket.off("new message", onNewMessage);
-    }
-
-    private void attemptSend() {
-        String message = mInputMessageView.getText().toString().trim();
-        if (TextUtils.isEmpty(message)) {
-            return;
-        }
-
-        mInputMessageView.setText("");
-        mSocket.emit("new message", message);
-    }
-
-    private Emitter.Listener onNewMessage = new Emitter.Listener() {
-        @Override
-        public void call(final Object... args) {
-            getActivity().runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    JSONObject data = (JSONObject) args[0];
-                    String username;
-                    String message;
-                    try {
-                        username = data.getString("username");
-                        message = data.getString("message");
-                    } catch (JSONException e) {
-                        return;
-                    }
-                    // add the message to view
-                    //addMessage(username, message);
-                }
-            });
-        }
-    };*/
 }
